38d3bf104fb1b344a0cad5ea719cb2aea6dfb83b
Tuhin Islam <tuhin981172@gmail.com>
2022-05-07 12:25:26 +0600

    add commit

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 
 const Products = () => {
+    const [products, setProducts] = useState()
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
+
+
         <div>
-            <h1>this is products</h1>
+            <h2>this is products</h2>
+            <div>
+                {
+                    products.map(product => <Product
+                        key={product._id}
+                        product={product}
+                    ></Product>)
+                }
+            </div>
         </div>
+
     );
 };
 
--- src/components/UploadProduct/UploadProduct.js
+++ src/components/UploadProduct/UploadProduct.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 const UploadProduct = () => {
+
+    const handleUpload = (event) => {
+        event.preventDefalut();
+
+        const name = event.target.name.value;
+        const price = event.target.price.value;
+        console.log(name, price);
+    }
+
     return (
-        <div>
-            <h1>this is upload product</h1>
+        <div className='container'>
+            <h2 className='text-center'> upload product</h2>
+
+            <div className='w-50 mx-auto'>
+                <form onSubmit={handleUpload}>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Product Name</label>
+                        <input type="text" name='name' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Price</label>
+                        <input type="text" name='price' class="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Upload</button>
+                </form>
+            </div>
         </div>
     );
 };
